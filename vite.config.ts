af600dab19de4000c44953b29a34e95b7eea5289
Tommy-Hck <huylebroeckt@gmail.com>
2026-05-13 09:53:49 +0200

    changing base name

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ function figmaAssetResolver() {
 
 export default defineConfig({
   // ⚠️ Remplace 'NOM-DU-REPO' par le nom exact de ton repo GitHub Pages
-  base: '/Devstarportfolio/',
+  base: '/2026portfolio/',
 
   plugins: [
     figmaAssetResolver(),
